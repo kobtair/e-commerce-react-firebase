26b81f14aabd5fabed2bd1b4c0c2a732b8717a2b
Aaqib Imran <aaqibimran0@gmail.com>
2023-09-12 13:34:04 +0500

    fixed sign out issue

--- src/contexts/UserContext/UserContext.jsx
+++ src/contexts/UserContext/UserContext.jsx
@@ -2,21 +2,17 @@ import { createContext, useState, useEffect } from "react";
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
-  signOutUser,
 } from "../../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
   currentUser: null,
-  setCurrentUser: () => null,
 });
 
 export const UserContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = {
     currentUser,
-    setCurrentUser,
   };
-  signOutUser();
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
@@ -28,3 +24,34 @@ export const UserContextProvider = ({ children }) => {
   }, []);
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+
+// import { createContext, useState, useEffect } from 'react';
+
+// import {
+//   onAuthStateChangedListener,
+//   createUserDocumentFromAuth,
+// } from '../utils/firebase/firebase.utils';
+
+// export const UserContext = createContext({
+//   setCurrentUser: () => null,
+//   currentUser: null,
+// });
+
+// export const UserProvider = ({ children }) => {
+//   const [currentUser, setCurrentUser] = useState(null);
+//   const value = { currentUser, setCurrentUser };
+
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChangedListener((user) => {
+//       if (user) {
+//         createUserDocumentFromAuth(user);
+//       }
+//       setCurrentUser(user);
+//     });
+
+//     return unsubscribe;
+//   }, []);
+
+//   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+// };
